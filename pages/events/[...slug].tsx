d49deacecc5b9dc495942a0fc5145ae59d584c70
Ayush Goyal <goyalayu@amazon.com>
2021-03-09 15:03:46 +0530

    add basic page routes

---
+++ pages/events/[...slug].tsx
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>The Filtered Events Page</h1>
+    </div>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/index.tsx
@@ -0,0 +1,9 @@
+const AllEventsPage = () => {
+  return (
+    <div>
+      <h1>The All Events Page</h1>
+    </div>
+  );
+};
+
+export default AllEventsPage;
